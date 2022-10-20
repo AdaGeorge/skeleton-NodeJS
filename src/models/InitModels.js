38850d3e3025d2089af829e3109693f244229b92
Ada George <adageorge79@gmail.com>
2022-10-19 19:21:53 -0500

    adding new models

--- src/models/InitModels.js
+++ src/models/InitModels.js
@@ -1,7 +1,13 @@
+const Categories = require('./categories.models')
+const Posts = require('./posts.models')
 const Users = require('./users.models') 
 
 const initModel = ()=>{
-    Users
+    Users.hasMany(Posts)
+    Posts.belongsTo(Users)
+    
+    Categories.hasMany(Posts)
+    Posts.belongsTo(Categories)
 }
 
 module.exports = initModel
